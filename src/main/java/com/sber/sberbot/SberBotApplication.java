6531e0cb72119d3c10988510cbcb64171725c5d0
Ian <yan4ik123456@gmail.com>
2023-10-27 14:26:08 +0600

    init commit

---
+++ src/main/java/com/sber/sberbot/SberBotApplication.java
@@ -0,0 +1,13 @@
+package com.sber.sberbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SberBotApplication.class, args);
+    }
+
+}
